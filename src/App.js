ba7228f95589f9b854de532b31e2ac347159d604
Luis Locon <luis.locon@gmail.com>
2021-09-20 22:39:36 -0600

    Use Reducer and Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Grudges from './Grudges';
+import NewGrudge from './NewGrudge';
 import './App.css';
 
-function App() {
+const Application = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Application">
+      <NewGrudge />
+      <Grudges />
     </div>
   );
-}
+};
 
-export default App;
+export default Application;
